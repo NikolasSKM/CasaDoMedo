a9e0cdd7af2955524577639bcc8dc1dbd3de7663
Primuwidi <44245731+Primuwidi@users.noreply.github.com>
2018-12-11 13:09:11 -0200

    CasaDoMedo

---
+++ CasaDoMedo/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaDoMedo
+{
+    class Program
+    {
+        string nome;
+        string nomeAmigo;
+
+        public static void Main()
+        {
+            Menu();
+            Console.ReadLine();
+        }
+
+        public static void Menu()
+        {
+            var enter = "";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("             +============================================+");
+            Console.WriteLine("             |              A Casa do Medo!               |");
+            Console.WriteLine("             |                  `'::::.                   |");
+            Console.WriteLine("             |                    _____Y_                 |");
+            Console.WriteLine("             |                   /      /                 |");
+            Console.WriteLine("             |                __/__/ __/  ___             |");
+            Console.WriteLine("             |            ---/__|  ::  | /___/ ----       |");
+            Console.WriteLine("             |               |;:|  ||  | |: ;||           |");
+            Console.WriteLine("             |                     ))                     |");
+            Console.WriteLine("             +============================================+");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("        +=====================================================+");
+            Console.WriteLine("        | Por favor, entre e se em casa... HAHAHAHAHAHAHA!!!! |");
+            Console.WriteLine("        +=====================================================+");
+            Console.WriteLine();
+            Console.WriteLine("                     Pressione ENTER para continuar!");
+            Console.ReadKey();
+            Console.Clear();
+
+            Nomes();
+        }//end of void menu
+
+        public static void Nomes()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("             +====================================+");
+            Console.WriteLine("             |Que nome quer dar ao seu personagem?|");
+            Console.WriteLine("             +====================================+");
+            nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("             +===============================================+");
+            Console.WriteLine("             |Beleza... Agora que nome quer dar ao seu amigo?|");
+            Console.WriteLine("             +===============================================+");
+            nomeAmigo = Console.ReadLine();
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("             +=================================================+");
+            Console.WriteLine("             |Ok!! Seu nome é " + nome + " , e do seu amigo é " + nomeAmigo + " |");
+            Console.WriteLine("             +=================================================+");
+            Console.Clear();
+
+            Console.WriteLine("             +===============================================+");
+            Console.WriteLine("             |Beleza... Agora vamos começar o joguinho...    |");
+            Console.WriteLine("             +===============================================+");
+            Console.Clear();
+
+            Cena1();
+        }//end of Nomes
+
+        public static void Cena1()
+        {
+            Console.WriteLine("             +===============================================+");
+            Console.WriteLine("             |" + Program.Nomes.nome + " é um jovem tranquilo, alto astral" );
+            Console.WriteLine("             +===============================================+");
+        }
+    }//end of class
+}//end of namespace
